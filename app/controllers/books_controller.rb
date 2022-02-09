32ca562e0cf1afacf0f5fdcbba3a5efacf46ef36
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-02-09 02:00:39 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,8 +26,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[ :notice] = "Book was successfully createed."
+    else
+      render :edit
+    end
   end
 
   def destroy
